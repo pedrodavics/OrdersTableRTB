d8b1e283e335a372ee0f84237cbaecff34c7e1d0
Pedro Davi <pedrodavi.cdesa@gmail.com>
2024-08-19 02:15:59 -0300

    Set up basic layout with navigation and styling in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav>
+      <router-link to="/">Orders</router-link>
+      <router-link to="/info">Info</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+nav {
+  background-color: #250dad;
+  padding: 10px;
+  display: flex;
+  justify-content: flex-end;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  margin-left: 10px;
+  padding: 5px 10px;
+  border-radius: 10px;
+  border: 2px solid white;
+  transition: border-width 0.15s ease-in-out;
+}
+nav a:hover {
+  border-width: 4px;
+}
+.active-link {
+  border-width: 4px;
 }
 </style>
+
